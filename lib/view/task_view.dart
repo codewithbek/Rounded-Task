0a2fff957ae3b13aaf27d3df27788f16a5b9cb26
Asadbek <106092435+AsadbekMobileDeveloper@users.noreply.github.com>
2022-11-29 22:29:56 +0500

    second commit

---
+++ lib/view/task_view.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+
+class TaskView extends StatefulWidget {
+  const TaskView({super.key});
+
+  @override
+  State<TaskView> createState() => _TaskViewState();
+}
+
+class _TaskViewState extends State<TaskView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
